a025c7cc9d7d296c4e409d5ca08edf01f72d4e6c
Andrey Milchakov <predator.alien.2017@mail.ru>
2026-04-03 10:26:50 +0500

    Setup functional REST API for Points with CORS for React

--- api/views.py
+++ api/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Point
+from .serializers import PointSerializer
 
-# Create your views here.
+class PointViewSet(viewsets.ModelViewSet):
+    queryset = Point.objects.all()
+    serializer_class = PointSerializer
