4dfb618eaf8622b66497fe537b5859929bcc9d82
Andrey Milchakov <predator.alien.2017@mail.ru>
2026-04-03 12:01:56 +0500

    Integrate Yandex Geocoder and point filtering system

--- api/views.py
+++ api/views.py
@@ -1,3 +1,4 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from .models import Point
 from .serializers import PointSerializer
@@ -5,3 +6,6 @@ from .serializers import PointSerializer
 class PointViewSet(viewsets.ModelViewSet):
     queryset = Point.objects.all()
     serializer_class = PointSerializer
+    filter_backends = [DjangoFilterBackend]
+    # Позволяем фильтровать по ID типов отходов
+    filterset_fields = ['accepted_waste']
